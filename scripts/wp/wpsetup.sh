42ea9c4db554d6503a7f7d24bc94573ef38e78c9
habe <ep.keitaro.habe@gmail.com>
2024-10-17 17:31:25 +0900

    php,mysql version

--- scripts/wp/wpsetup.sh
+++ scripts/wp/wpsetup.sh
@@ -13,5 +13,9 @@ mysql -h mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $WP_DBNAME CHA
 
 wp core config --dbname=$WP_DBNAME --dbuser=root --dbpass=$MYSQL_ROOT_PASSWORD --dbhost=mysql --dbprefix=wp_
 wp core install --url=http://$WEB_HOST_NAME --title=$WP_DBNAME --admin_user=$WP_USERNAME --admin_password=$WP_PASSWORD --admin_email=wp@$WEB_HOST_NAME
+
+wp plugin install classic-editor query-monitor duplicate-post --activate
+
 cp /scripts/wp/wp.gitignore ./.gitignore
+
 curl -L https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php > adminer.php
